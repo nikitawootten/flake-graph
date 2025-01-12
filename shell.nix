78db11c78ee6ae3a380cf9c1db2c6a9352e42014
Nikita Wootten <me@nikita.computer>
2025-01-11 20:46:06 -0500

    chore: Add GDB to shell.nix
    - Update flake.lock
    - Add watch rules to envrc

--- shell.nix
+++ shell.nix
@@ -1,12 +1,7 @@
 { pkgs ? import ./pkgs.nix }:
 pkgs.mkShell {
   inputsFrom = [ (pkgs.callPackage ./default.nix { }) ];
-  buildInputs = with pkgs; [
-    rust-analyzer
-    rustfmt
-    clippy
-    graphviz
-  ];
+  buildInputs = with pkgs; [ rust-analyzer rustfmt clippy graphviz gdb ];
   shellHook = ''
     export RUST_BACKTRACE="1"
   '';
