9ac50622982511c5873cc805642a7bc59082ea4b
Nikita Wootten <me@nikita.computer>
2026-01-14 23:22:01 -0500

    misc: Fix devshell on Darwin

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import ./pkgs.nix }:
 pkgs.mkShell {
   inputsFrom = [ (pkgs.callPackage ./default.nix { }) ];
-  buildInputs = with pkgs; [ rust-analyzer rustfmt clippy graphviz gdb ];
+  buildInputs = with pkgs; [ rust-analyzer rustfmt clippy graphviz ] ++ lib.lists.optionals stdenv.isLinux [ pkgs.gdb ];
   shellHook = ''
     export RUST_BACKTRACE="1"
   '';
